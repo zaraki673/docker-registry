0b2018b8c2513dcab7bb25399ed4701c2a5ec8eb
Olivier Gambier <olivier@docker.com>
2014-08-26 16:52:44 -0700

    Style
    Docker-DCO-1.1-Signed-off-by: Olivier Gambier <olivier@docker.com> (github: dmp42)

--- docker_registry/lib/config.py
+++ docker_registry/lib/config.py
@@ -99,7 +99,7 @@ def load():
 
     _config = Config(f.read())
     if flavor:
-        if not flavor in _config:
+        if flavor not in _config:
             raise exceptions.ConfigError(
                 'The specified flavor (%s) is missing in your config file (%s)'
                 % (flavor, config_path))
